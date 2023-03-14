5f920c23f11784c035f81517ead4cf38f0947c2f
Eric Olsen <126840543+eccol@users.noreply.github.com>
2023-03-14 19:12:37 -0400

    User set grid size

--- script.js
+++ script.js
@@ -1,21 +1,31 @@
-const GRID_WIDTH = 4;
-const GRID_HEIGHT = 4
-
 const container = document.querySelector('.container');
+document.querySelector('#start').addEventListener('click', createGrid);
 
 // Create grid
 
-for (let i = 0; i < GRID_HEIGHT; i++) {
-  for (let j = 0; j < GRID_WIDTH; j++) {
-    const square = document.createElement('div');
-    square.classList.add('square');
-    container.appendChild(square);
+function createGrid() {
+  const gridSize = prompt("What size grid? 1-100");
+
+  for (let i = 0; i < gridSize; i++) {
+    const row = document.createElement('div');
+    row.classList.add('row');
+    for (let j = 0; j < gridSize; j++) {
+      const square = document.createElement('div');
+      square.classList.add('square');
+      row.appendChild(square);
+    }
+    container.appendChild(row);
   }
+
+  // Respond to hover
+  const squares = document.querySelectorAll('.square');
+  squares.forEach((sq) => sq.addEventListener('mouseenter', lightUp));
+
 }
 
-// Respond to hover
-const squares = document.querySelectorAll('.square');
-squares.forEach((sq) => sq.addEventListener('mouseenter', lightUp));
+
+
+
 
 function lightUp(e) {
   // 'hovered' class in CSS adds the effect
