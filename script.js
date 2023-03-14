8c6ef638dba65c231fa5298d6aa4ec23f54f1e55
Eric Olsen <126840543+eccol@users.noreply.github.com>
2023-03-14 18:46:15 -0400

    Add hover effect

--- script.js
+++ script.js
@@ -3,11 +3,21 @@ const GRID_HEIGHT = 4
 
 const container = document.querySelector('.container');
 
+// Create grid
+
 for (let i = 0; i < GRID_HEIGHT; i++) {
   for (let j = 0; j < GRID_WIDTH; j++) {
     const square = document.createElement('div');
     square.classList.add('square');
     container.appendChild(square);
-    console.log(`Created square ${i} ${j}`)
   }
+}
+
+// Respond to hover
+const squares = document.querySelectorAll('.square');
+squares.forEach((sq) => sq.addEventListener('mouseenter', lightUp));
+
+function lightUp(e) {
+  // 'hovered' class in CSS adds the effect
+  e.target.classList.add('hovered');
 }
